20845c35adf5d86cdfeb44126ce75e107ed6a72a
andrismh <andrisyahputramuda@gmail.com>
2025-07-28 21:03:59 +0700

    restructured the repo and changes on app.js

--- server/app.js
+++ server/app.js
@@ -8,14 +8,25 @@ const port = 3000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "..", "views"));
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.render("index", {
+        name: "Mood"
+    });
 });
 
-app.get("/about.html", (req, res) => {
-    res.sendFile(path.join(__dirname, "../about.html"));
+app.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public/static/about.html"));
+});
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.listen(port, () =>{
     console.log(`Server running on port ${port}.`);
-});
+}); 
