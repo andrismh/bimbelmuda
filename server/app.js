94d33a2ec1bb09aae0d7ab2414e8e3b8f8950b92
andrismh <andrisyahputramuda@gmail.com>
2025-09-02 08:56:30 +0700

    refactor static sites

--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ app.use((err, req, res, next) => {
 const start = async () => {
   try {
     await connect();
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running at http://localhost:${PORT}`);
     });
   } catch (err) {
--- server/routes/mainRoute.js
+++ server/routes/mainRoute.js
@@ -1,14 +1,18 @@
 // routes/index.js
-import { Router } from 'express';
+import { Router } from "express";
 const mainRouter = Router();
 import {
-    getHomePage, getProjectsPage, getWritingsPage, getCreatePostPage, getAboutPage
-} from '../controllers/mainController.js';
+  getHomePage,
+  getProjectsPage,
+  getWritingsPage,
+  getCreatePostPage,
+  getAboutPage,
+} from "../controllers/mainController.js";
 
-mainRouter.get('/', getHomePage);
-mainRouter.get('/projects', getProjectsPage);
-mainRouter.get('/writings', getWritingsPage);
-mainRouter.get('/createPost', getCreatePostPage);
-mainRouter.get('/about', getAboutPage);
+mainRouter.get("/", getHomePage);
+mainRouter.get("/projects", getProjectsPage);
+mainRouter.get("/writings", getWritingsPage);
+mainRouter.get("/createPost", getCreatePostPage);
+mainRouter.get("/about", getAboutPage);
 
 export default mainRouter;
